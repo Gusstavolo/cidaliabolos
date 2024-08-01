da017628d5415062b05c08b6adebd45b446dc74f
Gustavo Oli <69ggustavoo69@gmail.com>
2024-08-01 11:05:58 -0300

    update overlayer

--- CidaliaBolos/src/components/Body.jsx
+++ CidaliaBolos/src/components/Body.jsx
@@ -1,6 +1,58 @@
 import '../App.css'
 import { useState,useEffect } from 'react'
+
+
+
 const OverlayLoad = () => {
+  const [visible, setVisible] = useState(false);
+  const [display, setDisplay] = useState(true);
+  useEffect(() => {
+    // Define o componente como visível ao montar
+    setVisible(true);
+    // Define o componente como não visível após 4 segundos
+    const timer = setTimeout(() => {
+      setVisible(false);
+    }, 1400);
+
+        if (!visible) {
+          setTimeout(() => {
+            
+              setDisplay(false);
+            
+          }, 2000);
+      }
+   
+    // Limpa o timer quando o componente é desmontado
+    return () => clearTimeout(timer);
+  }, []); 
+ 
+    return (
+      <>
+          { display && <div className={`OverlayLoadMain ${visible ? 'visible' : 'hidden'}`} >
+            
+            
+            <div className='Overlayinfo'>
+                <div className='cupA'></div>
+                Consulte o WhatsApp para mais informações.
+            </div>
+            <div className='Overlay-Insta'>
+              @andrade.cidalia
+            </div>
+        </div>
+    }
+    </>
+  )
+}
+const WhatsButton = ( ) => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const mensagem = "Olá, gostaria de encomendar um bolo.";
+    const numeroWhatsApp = '557599808083';
+    const urlWhatsApp = `whatsapp://send?phone=${numeroWhatsApp}&text=${encodeURIComponent(mensagem)}`;
+    
+    window.location.href = urlWhatsApp;
+  };
+
   const [visible, setVisible] = useState(false);
 
   useEffect(() => {
@@ -10,35 +62,25 @@ const OverlayLoad = () => {
     // Define o componente como não visível após 4 segundos
     const timer = setTimeout(() => {
       setVisible(false);
-    }, 2000);
+    }, 1400);
 
     // Limpa o timer quando o componente é desmontado
     return () => clearTimeout(timer);
-  }, []); 
-    return (
-      
-          <div className={`OverlayLoadMain ${visible ? 'visible' : 'hidden'}`}>
-            <div className='Overlayinfo'>
-                <div className='cupA'></div>
-                Consulte o WhatsApp para mais informações.
-            </div>
+  }, []);
+  
+  return (
 
-        </div>
-       
-    )
+        <button onSubmit={handleSubmit} className={`learn-more button-w ${visible ? 'off' : 'on'}`}> 
+                <div className='iconWats'></div>
+                FAÇA SEU PEDIDO!
+            </button>
+
+  )
 }
-
 
 export const Body = () => {
     
-    const handleSubmit = (event) => {
-      event.preventDefault();
-      const mensagem = "Olá, gostaria de encomendar um bolo.";
-      const numeroWhatsApp = '557599808083';
-      const urlWhatsApp = `whatsapp://send?phone=${numeroWhatsApp}&text=${encodeURIComponent(mensagem)}`;
-      
-      window.location.href = urlWhatsApp;
-    };
+    
 
     return(
 
@@ -49,10 +91,7 @@ export const Body = () => {
 
 
 
-            <button onSubmit={handleSubmit} className="learn-more button-w"> 
-                <div className='iconWats'></div>
-                FAÇA SEU PEDIDO!
-            </button>
+            <WhatsButton />
         </div>
     )
 }
